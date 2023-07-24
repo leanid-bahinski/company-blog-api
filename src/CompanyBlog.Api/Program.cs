63ad349c200f5235ee2d574547834a90f27316bf
leanid-bahinski <123372505+leanid-bahinski@users.noreply.github.com>
2023-07-24 10:19:43 +0200

    Refactoring project structure

--- src/CompanyBlog.Api/Program.cs
+++ src/CompanyBlog.Api/Program.cs
@@ -1,12 +1,12 @@
 using Microsoft.EntityFrameworkCore;
-using company_blog.Models;
+using CompanyBlog.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
-builder.Services.AddDbContext<BlogContext>(opt => opt.UseInMemoryDatabase("Blog"));
+builder.Services.AddDbContext<BlogContext>(opt => opt.UseInMemoryDatabase("CompanyBlog"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
